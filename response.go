cfe97e0ac71f3345a87d17fa0fab9c790a7f557e
Mosen <mosen@users.noreply.github.com>
2016-06-20 22:37:02 +1000

    Add all DeviceInformation response fields.

--- response.go
+++ response.go
@@ -10,8 +10,82 @@ type Response struct {
 	QueryResponses QueryResponses `json:"query_responses,omitempty" plist:",omitempty"`
 }
 
+type CommonQueryResponses struct {
+	UDID                          string
+	Languages                     []string          // ATV 6+
+	Locales                       []string          // ATV 6+
+	DeviceID                      string            // ATV 6+
+	OrganizationInfo              map[string]string // IOS 7+
+	LastCloudBackupDate           string            // IOS 8+
+	AwaitingConfiguration         bool              // IOS 9+
+							// iTunes
+	iTunesStoreAccountIsActive    bool              // IOS 7+ OSX 10.9+
+	iTunesStoreAccountHash        string            // IOS 8+ OSX 10.10+
+
+							// Device
+	DeviceName                    string
+	OSVersion                     string
+	BuildVersion                  string
+	ModelName                     string
+	Model                         string
+	ProductName                   string
+	SerialNumber                  string
+	DeviceCapacity                float32
+	AvailableDeviceCapacity       float32
+	BatteryLevel                  float32           // IOS 5+
+	CellularTechnology            int               // IOS 4+
+	IsSupervised                  bool              // IOS 6+
+	IsDeviceLocatorServiceEnabled bool              // IOS 7+
+	IsActivationLockEnabled       bool              // IOS 7+ OSX 10.9+
+	IsDoNotDisturbInEffect        bool              // IOS 7+
+	EASDeviceIdentifier           string            // IOS 7 OSX 10.9
+	IsCloudBackupEnabled          bool              // IOS 7.1
+
+							// Network
+	BluetoothMAC                  string
+	WiFiMAC                       string
+	EthernetMACs                  []string          // Surprisingly works in IOS
+}
+
+type AtvQueryResponses struct {
+
+}
+
+type IosQueryResponses struct {
+	IMEI                     string
+	MEID                     string
+	ModemFirmwareVersion     string
+	IsMDMLostModeEnabled     bool   // IOS 9.3
+	MaximumResidentUsers     int    // IOS 9.3
+
+					// Network
+	ICCID                    string // IOS
+	CurrentCarrierNetwork    string
+	SIMCarrierNetwork        string
+	SubscriberCarrierNetwork string
+	CarrierSettingsVersion   string
+	PhoneNumber              string
+	VoiceRoamingEnabled      bool
+	DataRoamingEnabled       bool
+	IsRoaming                bool
+	PersonalHotspotEnabled   bool
+	SubscriberMCC            string
+	SubscriberMNC            string
+	CurrentMCC               string
+	CurrentMNC               string
+}
+
+type MacosQueryResponses struct {
+	OSUpdateSettings   map[string]string // OSX 10.11+
+	LocalHostName      string            // OSX 10.11
+	HostName           string            // OSX 10.11
+	ActiveManagedUsers []string          // OSX 10.11
+}
+
 // QueryResponses is a DeviceInformation MDM Command Response
 type QueryResponses struct {
-	Model       string
-	ProductName string
+	CommonQueryResponses
+	MacosQueryResponses
+	IosQueryResponses
+	AtvQueryResponses
 }

44fb559290a1e2db244a476ed4675c9fc7820e5b
Mosen <mosen@users.noreply.github.com>
2016-06-22 15:02:40 +1000

    Add struct tags for json encoding of QueryResponses.

--- response.go
+++ response.go
@@ -12,40 +12,40 @@ type Response struct {
 
 // CommonQueryResponses has a list of query responses common to all device types
 type CommonQueryResponses struct {
-	UDID                  string
-	Languages             []string          // ATV 6+
-	Locales               []string          // ATV 6+
-	DeviceID              string            // ATV 6+
-	OrganizationInfo      map[string]string // IOS 7+
-	LastCloudBackupDate   string            // IOS 8+
-	AwaitingConfiguration bool              // IOS 9+
+	UDID                  string            `json:"udid"`
+	Languages             []string          `json:"languages,omitempty"`              // ATV 6+
+	Locales               []string          `json:"locales,omitempty"`                // ATV 6+
+	DeviceID              string            `json:"device_id"`                        // ATV 6+
+	OrganizationInfo      map[string]string `json:"organization_info,omitempty"`      // IOS 7+
+	LastCloudBackupDate   string            `json:"last_cloud_backup_date,omitempty"` // IOS 8+
+	AwaitingConfiguration bool              `json:"awaiting_configuration"`           // IOS 9+
 	// iTunes
-	ITunesStoreAccountIsActive bool   // IOS 7+ OSX 10.9+
-	ITunesStoreAccountHash     string // IOS 8+ OSX 10.10+
+	ITunesStoreAccountIsActive bool   `json:"itunes_store_account_is_active"` // IOS 7+ OSX 10.9+
+	ITunesStoreAccountHash     string `json:"itunes_store_account_hash"`      // IOS 8+ OSX 10.10+
 
 	// Device
-	DeviceName                    string
-	OSVersion                     string
-	BuildVersion                  string
-	ModelName                     string
-	Model                         string
-	ProductName                   string
-	SerialNumber                  string
-	DeviceCapacity                float32
-	AvailableDeviceCapacity       float32
-	BatteryLevel                  float32 // IOS 5+
-	CellularTechnology            int     // IOS 4+
-	IsSupervised                  bool    // IOS 6+
-	IsDeviceLocatorServiceEnabled bool    // IOS 7+
-	IsActivationLockEnabled       bool    // IOS 7+ OSX 10.9+
-	IsDoNotDisturbInEffect        bool    // IOS 7+
-	EASDeviceIdentifier           string  // IOS 7 OSX 10.9
-	IsCloudBackupEnabled          bool    // IOS 7.1
+	DeviceName                    string  `json:"device_name"`
+	OSVersion                     string  `json:"os_version"`
+	BuildVersion                  string  `json:"build_version"`
+	ModelName                     string  `json:"model_name"`
+	Model                         string  `json:"model"`
+	ProductName                   string  `json:"product_name"`
+	SerialNumber                  string  `json:"serial_number"`
+	DeviceCapacity                float32 `json:"device_capacity"`
+	AvailableDeviceCapacity       float32 `json:"available_device_capacity"`
+	BatteryLevel                  float32 `json:"battery_level,omitempty"`           // IOS 5+
+	CellularTechnology            int     `json:"cellular_technology,omitempty"`     // IOS 4+
+	IsSupervised                  bool    `json:"is_supervised"`                     // IOS 6+
+	IsDeviceLocatorServiceEnabled bool    `json:"is_device_locator_service_enabled"` // IOS 7+
+	IsActivationLockEnabled       bool    `json:"is_activation_lock_enabled"`        // IOS 7+ OSX 10.9+
+	IsDoNotDisturbInEffect        bool    `json:"is_dnd_in_effect"`                  // IOS 7+
+	EASDeviceIdentifier           string  `json:"eas_device_identifier"`             // IOS 7 OSX 10.9
+	IsCloudBackupEnabled          bool    `json:"is_cloud_backup_enabled"`           // IOS 7.1
 
 	// Network
-	BluetoothMAC string
-	WiFiMAC      string
-	EthernetMACs []string // Surprisingly works in IOS
+	BluetoothMAC string   `json:"bluetooth_mac"`
+	WiFiMAC      string   `json:"wifi_mac"`
+	EthernetMACs []string `json:"ethernet_macs"` // Surprisingly works in IOS
 }
 
 // AtvQueryResponses contains AppleTV QueryResponses
@@ -54,49 +54,49 @@ type AtvQueryResponses struct {
 
 // IosQueryResponses contains iOS QueryResponses
 type IosQueryResponses struct {
-	IMEI                 string
-	MEID                 string
-	ModemFirmwareVersion string
-	IsMDMLostModeEnabled bool // IOS 9.3
-	MaximumResidentUsers int  // IOS 9.3
+	IMEI                 string `json:"imei"`
+	MEID                 string `json:"meid"`
+	ModemFirmwareVersion string `json:"modem_firmware_version"`
+	IsMDMLostModeEnabled bool   `json:"is_mdm_lost_mode_enabled,omitempty"` // IOS 9.3
+	MaximumResidentUsers int    `json:"maximum_resident_users"`             // IOS 9.3
 
 	// Network
-	ICCID                    string // IOS
-	CurrentCarrierNetwork    string
-	SIMCarrierNetwork        string
-	SubscriberCarrierNetwork string
-	CarrierSettingsVersion   string
-	PhoneNumber              string
-	VoiceRoamingEnabled      bool
-	DataRoamingEnabled       bool
-	IsRoaming                bool
-	PersonalHotspotEnabled   bool
-	SubscriberMCC            string
-	SubscriberMNC            string
-	CurrentMCC               string
-	CurrentMNC               string
+	ICCID                    string `json:"iccid,omitempty"` // IOS
+	CurrentCarrierNetwork    string `json:"current_carrier_network,omitempty"`
+	SIMCarrierNetwork        string `json:"sim_carrier_network,omitempty"`
+	SubscriberCarrierNetwork string `json:"subscriber_carrier_network,omitempty"`
+	CarrierSettingsVersion   string `json:"carrier_settings_version,omitempty"`
+	PhoneNumber              string `json:"phone_number,omitempty"`
+	VoiceRoamingEnabled      bool   `json:"voice_roaming_enabled,omitempty"`
+	DataRoamingEnabled       bool   `json:"data_roaming_enabled,omitempty"`
+	IsRoaming                bool   `json:"is_roaming,omitempty"`
+	PersonalHotspotEnabled   bool   `json:"personal_hotspot_enabled,omitempty"`
+	SubscriberMCC            string `json:"subscriber_mcc,omitempty"`
+	SubscriberMNC            string `json:"subscriber_mnc,omitempty"`
+	CurrentMCC               string `json:"current_mcc,omitempty"`
+	CurrentMNC               string `json:"current_mnc,omitempty"`
 }
 
 // OSUpdateSettingsResponse contains information about macOS update settings.
 type OSUpdateSettingsResponse struct {
-	AutoCheckEnabled	bool
-	AutomaticAppInstallationEnabled	bool
-	AutomaticOSInstallationEnabled bool
-	AutomaticSecurityUpdatesEnabled bool
-	BackgroundDownloadEnabled bool
-	CatalogURL	string
-	IsDefaultCatalog	bool
-	PerformPeriodicCheck	bool
-	PreviousScanDate	string
-	PreviousScanResult	int
+	AutoCheckEnabled                bool   `json:"auto_check_enabled"`
+	AutomaticAppInstallationEnabled bool   `json:"automatic_app_installation_enabled"`
+	AutomaticOSInstallationEnabled  bool   `json:"automatic_os_installation_enabled"`
+	AutomaticSecurityUpdatesEnabled bool   `json:"automatic_security_updates_enabled"`
+	BackgroundDownloadEnabled       bool   `json:"background_download_enabled"`
+	CatalogURL                      string `json:"catalog_url"`
+	IsDefaultCatalog                bool   `json:"is_default_catalog"`
+	PerformPeriodicCheck            bool   `json:"perform_periodic_check"`
+	PreviousScanDate                string `json:"previous_scan_date"`
+	PreviousScanResult              int    `json:"previous_scan_result"`
 }
 
 // MacosQueryResponses contains macOS queryResponses
 type MacosQueryResponses struct {
 	OSUpdateSettings   OSUpdateSettingsResponse // OSX 10.11+
-	LocalHostName      string            // OSX 10.11
-	HostName           string            // OSX 10.11
-	ActiveManagedUsers []string          // OSX 10.11
+	LocalHostName      string                   `json:"local_host_name,omitempty"` // OSX 10.11
+	HostName           string                   `json:"host_name,omitempty"`       // OSX 10.11
+	ActiveManagedUsers []string                 `json:"active_managed_users"`      // OSX 10.11
 }
 
 // QueryResponses is a DeviceInformation MDM Command Response
